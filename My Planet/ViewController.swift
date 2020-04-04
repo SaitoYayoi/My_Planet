48eace4a7ef72fc001da6ebf5837ca0ca31e8f41
SaitoYayoi <hugj2376@gmail.com>
2020-04-04 17:49:40 +0800

    Initial Commit

---
+++ My Planet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Planet
+//
+//  Created by SaitoYayoi on 2020/4/4.
+//  Copyright © 2020 KanoYuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
